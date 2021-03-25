1021c3d239d99332d8e8c2e298cbb5554cfa6765
DAN3002 <dinhanh300229@gmail.com>
2021-03-25 15:19:06 +0700

    [ADD] Add eslint and husky

--- client/main.js
+++ client/main.js
@@ -4,19 +4,19 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+	// counter starts at 0
+	this.counter = new ReactiveVar(0);
 });
 
 Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
+	counter() {
+		return Template.instance().counter.get();
+	},
 });
 
 Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+	'click button'(event, instance) {
+		// increment the counter when button is clicked
+		instance.counter.set(instance.counter.get() + 1);
+	},
 });
