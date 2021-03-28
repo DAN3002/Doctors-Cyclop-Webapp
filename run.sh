ba08b8ebd6c1edc96ae2033986b1662b065591a8
DAN3002 <dinhanh300229@gmail.com>
2021-03-28 14:26:55 +0700

    [ADD] Add run script and config DB

---
+++ run.sh
@@ -0,0 +1,7 @@
+export NODE_ENV=development \
+export PORT=3000 \
+export ROOT_URL=http://localhost:3000 \
+export MONGO_URL=mongodb://localhost:27017/doctor-cylop \
+export MONGO_OPLOG_URL=mongodb://localhost:27017/local \
+
+meteor
