2fa539416ff7b22177c32b6ec62f3c2e55c9cd2b
DAN3002 <dinhanh300229@gmail.com>
2021-04-03 19:59:53 +0700

    [ADD] Add filter by query of url

---
+++ app/enum/STATE.js
@@ -0,0 +1,7 @@
+export const STATE = [
+	'All',
+	'Waiting',
+	'Normal',
+	'Abnormal',
+	'Borderline',
+];
--- app/ui/client/view/patientList.js
+++ app/ui/client/view/patientList.js
@@ -1,13 +1,25 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+import { FlowRouter } from 'meteor/kadira:flow-router';
 
 import { Cases } from '../../../model';
 import { getUserEmail } from '../../../lib/client/getUserEmail';
+import { STATE } from '../../../enum/STATE';
 import './patientList.html';
 
 Template.patientList.helpers({
 	casesList() {
 		return Template.instance().casesList.get();
+	},
+	states() {
+		return STATE;
+	},
+	isSelectedState(currentState) {
+		const { state } = FlowRouter.current().queryParams;
+		return state === currentState;
+	},
+	filterQuery() {
+		return FlowRouter.current().queryParams;
 	},
 });
 
@@ -31,7 +43,7 @@ Template.patientList.events({
 
 Template.patientList.onCreated(function() {
 	this.casesList = new ReactiveVar([]);
-	this.filter = new ReactiveVar({});
+	this.filter = new ReactiveVar(FlowRouter.current().queryParams);
 
 	const caseSub = this.subscribe('caseByDoctorEmail', getUserEmail());
 	this.autorun(() => {
