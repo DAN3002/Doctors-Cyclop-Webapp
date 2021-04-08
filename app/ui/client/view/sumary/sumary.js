46270cea1209bdf9ca75bb721916c24730396824
DAN3002 <dinhanh300229@gmail.com>
2021-04-08 17:54:07 +0700

    [ADD] Init chart

--- app/ui/client/view/sumary/sumary.js
+++ app/ui/client/view/sumary/sumary.js
@@ -1,1 +1,36 @@
+import {
+	Template,
+} from 'meteor/templating';
+
+import { Chart } from '../../../../../node_modules/chart.js/dist/chart.min';
 import './sumary.html';
+
+Template.sumary.onRendered(function() {
+	const labels = [
+		'January',
+		'February',
+		'March',
+		'April',
+		'May',
+		'June',
+	];
+	const data = {
+		labels,
+		datasets: [{
+			label: 'My First dataset',
+			backgroundColor: 'rgb(255, 99, 132)',
+			borderColor: 'rgb(255, 99, 132)',
+			data: [0, 10, 5, 2, 20, 30, 45],
+		}],
+	};
+	const config = {
+		responsive: true,
+		type: 'line',
+		data,
+		options: {},
+	};
+	new Chart(
+		document.getElementById('myChart'),
+		config,
+	);
+});
